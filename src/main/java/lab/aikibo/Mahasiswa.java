b5b217dbcfd8970beb023be9e5a0fb13ac7a2c26
tamami <tamami.oka@gmail.com>
2017-03-26 23:09:40 +0700

    init comit

---
+++ src/main/java/lab/aikibo/Mahasiswa.java
@@ -0,0 +1,17 @@
+package lab.aikibo;
+
+import java.util.Date;
+
+/**
+ * Created by tamami on 26/03/17.
+ */
+public class Mahasiswa {
+
+    private String nim;
+    private String nama;
+    private String tempatLahir;
+    private Date tanggalLahir;
+    private boolean jenisKelamin; // true = pria
+    private String alamat;
+
+}
